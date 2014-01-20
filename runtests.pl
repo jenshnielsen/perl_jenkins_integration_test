13168828ceb42e7a1ee8728e352c5627d522c4dc
Jens H. Nielsen <jens.nielsen@ucl.ac.uk>
2014-01-20 16:09:42 +0000

    Use Junit harness because junit formatter is not on RHEL

--- runtests.pl
+++ runtests.pl
@@ -1,10 +1,10 @@
 #! /usr/bin/env perl
 use 5.10.0;
 use strict;
-use TAP::Harness;
+use TAP::Harness::JUnit;
 
-my %args = (verbosity => 1,);
-my $harness = TAP::Harness->new( \%args );
+my %args = (verbosity => 1, xmlfile => 'output.xml');
+my $harness = TAP::Harness::JUnit->new( \%args );
 #my @tests = ['t/simple_test.pl'];
 my @tests = glob('t/*.t');
 $harness->runtests(@tests);
